891c01d2e802b0d03c203babc8458ad84f5b2901
Fernanda5510 <130413112+Fernanda5510@users.noreply.github.com>
2023-04-17 21:41:43 -0300

    Update AreaTriangulo(Classe).cpp

--- AreaTriangulo(Classe).cpp
+++ AreaTriangulo(Classe).cpp
@@ -2,26 +2,46 @@
 #include <cmath>
 using namespace std;
 
-//funcao que ira receber o valor dos dados e retornar a area dos triangulos
-double CalcularAreaTri(double a, double b, double c){
-    double p = (a + b + c)/2;
-    return sqrt(p * (p - a) * (p - b) * (p - c));//TESTE
-}
+//classe diferente da principal
+class Triangulo {
+private:
+    double a, b, c;
+public:
+    Triangulo (double lado1, double lado2, double lado3){
+        a = lado1;
+        b = lado2;
+        c = lado3;
+    }
+    
+    //funcao que ira receber o valor dos dados e retornar a area dos triangulos
+    double calculaArea(){
+        double p = (a + b + c) /2.0;
+        double area = sqrt(p * (p - a) * (p - b) * (p - c));
+        return area;
+    }
+    
+    //proxima aula
+    double getLado1() {return a;}
+    double getLado2() {return b;}
+    double getLado3() {return c;}
+};
 
 int main() {
     double xa, xb, xc, ya, yb, yc;
     
-    //interface para o usuario e armazenamento dos tamanho dos lados digitados
+    //interface para o usuario e armazenamento dos tamanho dos lados digitados criando um objecto da classe triangulo com as informacoes fornecidas
     cout << "Digite as medidas do triangulo X: " << endl;
     cin >> xa >> xb >> xc;
+    Triangulo X(xa, xb, xc);
     
-    //interface para o usuario e armazenamento dos tamanho dos lados digitados
+    //interface para o usuario e armazenamento dos tamanho dos lados digitados criando um objecto da classe triangulo com as informacoes fornecidas
     cout << "Digite as medidas do triangulo Y: " << endl;
     cin >> ya >> yb >> yc;
+    Triangulo Y(ya, yb, yc);
     
-    //armazenamento do valor retornado pela funcao da area na funcao principal
-    double areax = CalcularAreaTri(xa,xb,xc);
-    double areay = CalcularAreaTri(ya,yb,yc);
+    //armazenamento do valor retornado pela funcao da area localizado na classe triangulo na funcao principal
+    double areax = X.calculaArea();
+    double areay = Y.calculaArea();
     
     //interface informando as areas do triangulo para o usuario
     cout << "Area triangulo X: " << areax << "\n" << "Area triangulo Y:" << areay << endl;
@@ -34,4 +54,5 @@ int main() {
     } else{
         cout << "Os dois triangulos tem a mesma area." << endl;
     }
+    return 0;
 }
